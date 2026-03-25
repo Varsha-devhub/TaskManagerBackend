2c53e34c06cc8042b8cba7d8f3f32ace72bf7479
Varsha K <varshakpayyannur@gmail.com>
2026-03-25 13:05:12 -0700

    fix: mailtrap email setup

--- config/utils/sendEmail.js
+++ config/utils/sendEmail.js
@@ -1,7 +1,9 @@
 const nodemailer=require("nodemailer");
 const sendEmail=async(options)=>{
     const transporter=nodemailer.createTransport({
-        service:"gmail",
+        
+         host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
         auth:{
             user:process.env.EMAIL_USER,
             pass:process.env.EMAIL_PASS
